48c2b342eddc45ad585aaf27ff971d9157c5f946
Alvin Galit <aroblesgalit@gmail.com>
2023-01-19 17:31:56 -0800

    Learned more about number and boolean and type inference

--- 01_Intro/variableMe.js
+++ 01_Intro/variableMe.js
@@ -1,2 +1,9 @@
+"use strict";
+exports.__esModule = true;
 var greetings = 'Hello Alvin';
+greetings.toLowerCase();
 console.log(greetings);
+// number
+var userID = 345;
+// boolean
+var isLoggedIn = false;
--- 01_Intro/variableMe.ts
+++ 01_Intro/variableMe.ts
@@ -1,9 +1,15 @@
 let greetings: string = 'Hello Alvin';
 
-let mynum = 6;
-
 greetings.toLowerCase();
-
 console.log(greetings);
+
+// number
+let userID: number = 345
+
+// boolean
+let isLoggedIn: boolean = false
+
+
+
 
 export {}
