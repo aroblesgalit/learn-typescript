61c84b0bbd1fad668b50d0d87e1849a699607855
Alvin Galit <aroblesgalit@gmail.com>
2023-01-19 18:03:01 -0800

    Learned of better ways to write functions - declaring type of return values

--- 02BASICS/myfunctions.ts
+++ 02BASICS/myfunctions.ts
@@ -1,4 +1,4 @@
-function addTwo(num: number) {
+function addTwo(num: number): number {
     return num + 2
 }
 
@@ -14,5 +14,32 @@ let myValue = addTwo(5)
 getUpper('hello')
 signUpUser('Alvin', 'alvin@co.dev', true)
 loginUser('Alvin', 'alvin@co.dev')
+
+function getValue(myVal: number): string | boolean {
+    if (myVal > 5) {
+        return true
+    }
+
+    return '200 OK'
+}
+
+const getHello = (s: string): string => {
+    return ''
+}
+
+const heros = ['Thor', 'Spiderman', 'Ironman']
+// const heros = [1, 2, 3]
+
+heros.map((hero): string => {
+    return `Hero is ${hero}`
+})
+
+function consoleError(errmsg: string): void {
+    console.log(errmsg);
+}
+
+function handleError(errmsg: string): never {
+    throw new Error(errmsg);
+}
 
 export {}
