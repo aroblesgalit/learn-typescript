0188b3c6b8f22f2e1d62cdf31581ec876b57364e
Alvin Galit <aroblesgalit@gmail.com>
2023-02-02 17:50:48 -0800

    Learned tuples

--- 02BASICS/myTupEnums.ts
+++ 02BASICS/myTupEnums.ts
@@ -1,1 +1,41 @@
-const user: (string | number)[] = ['ag', 1]
+// const user: (string | number)[] = ['ag', 1]
+let tUser: [string, number, boolean]
+
+tUser = ['ag', 131, true]
+
+let rgb: [number, number, number] = [255, 123, 112]
+
+type User = [number, string]
+
+const newUser: User = [112, 'ag@email.com']
+
+newUser[1] = 'ag@dev.co'
+newUser.push(true)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export {}
