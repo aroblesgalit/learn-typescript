eb2c94d9851a3841af5fddf8c3c95c49db66d5b1
Alvin Galit <aroblesgalit@gmail.com>
2023-02-06 18:32:30 -0800

    Learned interface

---
+++ 02BASICS/myInterface.ts
@@ -0,0 +1,24 @@
+interface User {
+    readonly dbId: number,
+    email: string,
+    userId: number,
+    googleId?: string,
+    // startTrial: () => string
+    startTrial(): string,
+    getCoupon(couponname: string, value: number): number
+}
+
+const alvin: User = {
+    dbId: 12,
+    email: 'alvin@dev.co',
+    userId: 1234,
+    startTrial: () => {
+        return 'Trial started'
+    },
+    getCoupon: (name: 'alvin', off: 10) => {
+        return 10
+    }
+}
+
+alvin.email = 'a@dev.co'
+// alvin.dbId = 13
