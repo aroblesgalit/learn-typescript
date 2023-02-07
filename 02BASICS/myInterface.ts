c5e10159bb00f902a0648ec9259cfeb01f187a53
Alvin Galit <aroblesgalit@gmail.com>
2023-02-06 18:38:22 -0800

    Learned inheritance and how you can extend to an interface

--- 02BASICS/myInterface.ts
+++ 02BASICS/myInterface.ts
@@ -8,14 +8,34 @@ interface User {
     getCoupon(couponname: string, value: number): number
 }
 
+// Reopen interface
 interface User {
     githubToken: string
+}
+
+// Inheritance
+interface Admin extends User {
+    role: 'admin' | 'ta' | 'learner'
 }
 
 const alvin: User = {
     dbId: 12,
     email: 'alvin@dev.co',
     userId: 1234,
+    githubToken: 'github',
+    startTrial: () => {
+        return 'Trial started'
+    },
+    getCoupon: (name: 'alvin', off: 10) => {
+        return 10
+    }
+}
+
+const admin: Admin = {
+    dbId: 12,
+    email: 'alvin@dev.co',
+    userId: 1234,
+    role: 'admin',
     githubToken: 'github',
     startTrial: () => {
         return 'Trial started'
