d3c86b467603443073c83d8cf452794cd3e9c884
Alvin Galit <aroblesgalit@gmail.com>
2023-02-15 22:20:00 -0800

    Learning about private and public

--- 03PURETS/dist/index.js
+++ 03PURETS/dist/index.js
@@ -1,10 +1,19 @@
 "use strict";
+// class User {
+//     public email: string
+//     private name: string
+//     readonly city: string = 'San Diego'
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
 class User {
     constructor(email, name) {
-        this.city = 'San Diego';
         this.email = email;
         this.name = name;
+        this.city = 'San Diego';
     }
 }
 const alvin = new User('alvin@dev.co', 'alvin');
-// alvin.city = 'San Diego'
+// alvin.name
--- 03PURETS/src/index.ts
+++ 03PURETS/src/index.ts
@@ -1,12 +1,22 @@
+// class User {
+//     public email: string
+//     private name: string
+//     readonly city: string = 'San Diego'
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
+
 class User {
-    email: string
-    name: string
     readonly city: string = 'San Diego'
-    constructor(email: string, name: string) {
-        this.email = email;
-        this.name = name;
+    constructor(
+        public email: string, 
+        public name: string,
+        // private userId: string
+        ){
     }
 }
 
 const alvin = new User('alvin@dev.co', 'alvin')
-// alvin.city = 'San Diego'
+// alvin.name
