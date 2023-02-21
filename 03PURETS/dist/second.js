1c77d640493a629b0beed190e3360d86226ff6c1
Alvin Galit <aroblesgalit@gmail.com>
2023-02-20 23:29:58 -0800

    Learned about abstract classes

--- 03PURETS/dist/second.js
+++ 03PURETS/dist/second.js
@@ -1,19 +1,6 @@
 "use strict";
-class Instagram {
-    constructor(cameraMode, filter, burst) {
-        this.cameraMode = cameraMode;
-        this.filter = filter;
-        this.burst = burst;
-    }
-}
-class Youtube {
-    constructor(cameraMode, filter, burst, short) {
-        this.cameraMode = cameraMode;
-        this.filter = filter;
-        this.burst = burst;
-        this.short = short;
-    }
-    createStory() {
-        console.log('Story was created');
-    }
-}
+// interface TakePhoto {
+//     cameraMode: string
+//     filter: string
+//     burst: number
+// }
