028e7979b505a476c80a82898dc47b2faa689920
Alvin Galit <aroblesgalit@gmail.com>
2023-02-22 22:26:33 -0800

    Learned about generics in arrays and arrow functions

--- 03PURETS/dist/myGenerics.js
+++ 03PURETS/dist/myGenerics.js
@@ -14,4 +14,14 @@ function identityThree(val) {
 function identityFour(val) {
     return val;
 }
-identityFour({});
+// identityFour<Bottle>({})
+function getSearchProducts(products) {
+    // do some database operations
+    const myIndex = 3;
+    return products[myIndex];
+}
+const getMoreSearchProducts = (products) => {
+    // do some database operations
+    const myIndex = 4;
+    return products[myIndex];
+};
--- 03PURETS/src/myGenerics.ts
+++ 03PURETS/src/myGenerics.ts
@@ -24,4 +24,16 @@ interface Bottle {
     type: number
 }
 
-identityFour<Bottle>({})
+// identityFour<Bottle>({})
+
+function getSearchProducts<T>(products: T[]): T {
+    // do some database operations
+    const myIndex = 3
+    return products[myIndex]
+}
+
+const getMoreSearchProducts = <T>(products: T[]): T => {
+    // do some database operations
+    const myIndex = 4
+    return products[myIndex]
+}
